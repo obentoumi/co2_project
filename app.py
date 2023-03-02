0aa4387e853c904d7f966fb4e80516e3d9a1580e
danieltummler <115984509+danielwtummler@users.noreply.github.com>
2023-03-02 14:06:56 +0100

    changes

--- app.py
+++ app.py
@@ -122,14 +122,14 @@ def main():
         if st.button(label = "Are you ready?"):
             st.balloons()
 
-    elif choice == "Exploratory Data Analysis":
-        eda_app()
+    # elif choice == "Exploratory Data Analysis":
+    #     eda_app()
 
-    elif choice == "Machine Learning Model":
-        ml_app()
+    # elif choice == "Machine Learning Model":
+    #     ml_app()
 
-    elif choice == "About":
-        about_app()
+    # elif choice == "About":
+    #     about_app()
 
 
 
