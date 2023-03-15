5f0c5792f914adbcaa9be237412569a978b387ab
danieltummler <115984509+danielwtummler@users.noreply.github.com>
2023-03-15 20:07:28 +0100

    changes

--- about.py
+++ about.py
@@ -4,11 +4,10 @@ import streamlit as st
 def about_app():
     st.subheader("Nothing Here! :no_pedestrians:")
 
-if __name__ == "__about_app__":
-    about_app()
+    # embedded_code = '<iframe title="Report Section" width="1100" height="560" src="https://app.powerbi.com/view?r=eyJrIjoiMzFmNmQ5NWEtZDkwNy00MzU0LThjZWItOTllMTg4MDI3ZWIwIiwidCI6IjE3MTA5MzMxLTUwYzUtNDc1ZS05YmI0LTNmNGMwNWQzYmI0OSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>'
 
-
-
+    # st.markdown(body = embedded_code,
+    #             unsafe_allow_html = True)
 # pip install pipreqs
 # pipreqs --encoding=utf8
 # pipreqs --encoding=utf8 --force
